9296d5b285172f547b4aa5021b9515d4b748d5bd
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-02 17:22:02 -0500

    day 2

---
+++ day 02/part1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using AdventCommon;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entries = ConsoleHelpers.ReadAllLines().Select(PasswordEntry.Parse);
+            Console.WriteLine(entries.Count(IsPasswordValid));
+        }
+
+        public class PasswordEntry
+        {
+            public int Min { get; set; }
+            public int Max { get; set; }
+            public char Character { get; set; }
+            public string Password { get; set; }
+
+            public static PasswordEntry Parse(string str)
+            {
+                var match = Regex.Match(str, @"^(\d+)-(\d+) (\w): (.*)$");
+
+                if (match == null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(str));
+                }
+
+                return new PasswordEntry
+                {
+                    Min = int.Parse(match.Groups[1].Value),
+                    Max = int.Parse(match.Groups[2].Value),
+                    Character = match.Groups[3].Value[0],
+                    Password = match.Groups[4].Value
+                };
+            }
+        }
+
+        static bool IsPasswordValid(PasswordEntry entry)
+        {
+            var count = entry.Password.Count(c => c == entry.Character);
+            return entry.Min <= count && entry.Max >= count;
+        }
+    }
+}
---
+++ day 02/part2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using AdventCommon;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day_02
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var entries = ConsoleHelpers.ReadAllLines().Select(PasswordEntry.Parse);
+            Console.WriteLine(entries.Count(IsPasswordValid));
+            return 0;
+        }
+
+        public class PasswordEntry
+        {
+            public int Position1 { get; set; }
+            public int Position2 { get; set; }
+            public char Character { get; set; }
+            public string Password { get; set; }
+
+            public static PasswordEntry Parse(string str)
+            {
+                var match = Regex.Match(str, @"^(\d+)-(\d+) (\w): (.*)$");
+
+                if (match == null)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(str));
+                }
+
+                return new PasswordEntry
+                {
+                    Position1 = int.Parse(match.Groups[1].Value),
+                    Position2 = int.Parse(match.Groups[2].Value),
+                    Character = match.Groups[3].Value[0],
+                    Password = match.Groups[4].Value
+                };
+            }
+        }
+
+        static bool IsPasswordValid(PasswordEntry entry)
+        {
+             var firstChar = entry.Password[entry.Position1 - 1];
+             var secondChar = entry.Password[entry.Position2 - 1];
+
+             return firstChar != secondChar && (firstChar == entry.Character || secondChar == entry.Character);
+        }
+    }
+}
