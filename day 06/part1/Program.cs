8f89ab6dad3baa455546769625ba672dc4b398b9
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-06 18:22:01 -0500

    day 6

---
+++ day 06/part1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var answer = string
+                .Join("\n", ConsoleHelpers.ReadInput())
+                .Split("\n\n")
+                .Select(x => x
+                    .Replace("\n", "")
+                    .Distinct()
+                    .Count())
+                .Aggregate(0, (prev, cur) => prev + cur);
+
+
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ day 06/part2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using AdventCommon;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = string
+                .Join("\n", ConsoleHelpers.ReadInput())
+                .Split("\n\n")
+                .Select(x => {
+                    var lines = x.Split("\n");
+                    var remaining = lines[0];
+
+                    if (lines.Length > 1) 
+                    {
+                        for (var i = 1; i < lines.Length; i++)
+                        {
+                            var line = lines[i];
+                            foreach (var q in remaining)
+                            {
+                                if (!line.Contains(q))
+                                {
+                                    remaining = remaining.Replace($"{q}", string.Empty);
+                                }
+                            }
+                        }
+                    }
+
+                    return remaining.Length;
+                })
+                .Aggregate(0, (prev, cur) => prev + cur);
+
+
+            Console.WriteLine(answer);
+        }
+    }
+}
