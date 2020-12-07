11477fdaf13434b4a86c3e10639c4e6ede9a179e
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-07 18:08:21 -0500

    day 7 - it's ugly and i hate it, but it works

---
+++ day 07/part1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using AdventCommon;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bagDefRegex = new Regex(@"(\d+) (.*?) bags?[,.]");
+            var lines = ConsoleHelpers.ReadInput();
+            var bagDefs = lines
+                .Select(line =>
+                {
+                    var parts = line.Split(" bags contain ");
+                    var matches = bagDefRegex.Matches(parts[1]);
+
+                    if (matches.Count == 0)
+                    {
+                        return new Bag
+                        {
+                            Color = parts[0],
+                            Contents = Array.Empty<string>()
+                        };
+                    }
+
+                    return new Bag
+                    {
+                        Color = parts[0],
+                        Contents = matches.Select(m => m.Groups[2].Value).ToArray()
+                    };
+                })
+                .ToDictionary(f => f.Color, f => f.Contents);
+
+                Console.WriteLine(bagDefs.Count(x => HasShinyGold(x.Key, bagDefs)));
+        }
+
+        public class Bag
+        {
+            public string Color { get; set; }
+            public string[] Contents { get; set; }
+        }
+
+        static bool HasShinyGold(string color, Dictionary<string, string[]> defs)
+        {
+            if (!defs.ContainsKey(color))
+            {
+                return false;
+            }
+    
+            if (defs[color].Any(d => d == "shiny gold"))
+            {
+                return true;
+            }
+
+            return defs[color].Any(d => HasShinyGold(d, defs));
+        }
+    }
+}
---
+++ day 07/part2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using AdventCommon;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bagDefRegex = new Regex(@"(\d+) (.*?) bags?[,.]");
+            var lines = ConsoleHelpers.ReadInput();
+            var bagDefs = lines
+                .Select(line =>
+                {
+                    var parts = line.Split(" bags contain ");
+                    var matches = bagDefRegex.Matches(parts[1]);
+
+                    if (matches.Count == 0)
+                    {
+                        return new Bag
+                        {
+                            Color = parts[0],
+                            Contents = null
+                        };
+                    }
+
+                    return new Bag
+                    {
+                        Color = parts[0],
+                        Contents = matches.ToDictionary(x => x.Groups[2].Value, x => int.Parse(x.Groups[1].Value))
+                    };
+                })
+                .ToDictionary(f => f.Color, f => f.Contents);
+
+                Console.WriteLine(CountBagsInside("shiny gold", bagDefs));
+        }
+
+        public class Bag
+        {
+            public string Color { get; set; }
+            public Dictionary<string, int> Contents { get; set; }
+        }
+
+        static int CountBagsInside(string color, Dictionary<string, Dictionary<string, int>> defs)
+        {
+            if (defs[color] == null)
+            {
+                return 0;
+            }
+
+            return defs[color].Sum(x => x.Value + (x.Value * CountBagsInside(x.Key, defs)));
+        }
+    }
+}
