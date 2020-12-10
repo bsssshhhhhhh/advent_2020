2b079b0722673c5464b947e05469410453e1c6f8
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-10 02:41:52 -0500

    day 10

---
+++ day 10/part1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adapterJoltages = ConsoleHelpers
+                .ReadInput()
+                .Select(int.Parse)
+                .ToList();
+            
+            adapterJoltages.Add(adapterJoltages.Max() + 3);
+            adapterJoltages.Add(0);
+            adapterJoltages.Sort();
+            
+            var differences = new List<int>();
+            for (var i = 0; i < adapterJoltages.Count - 1; i++)
+            {
+                differences.Add(adapterJoltages[i + 1] - adapterJoltages[i]);
+            }
+
+
+            Console.WriteLine(differences.Count(x => x == 1) * differences.Count(x => x == 3));
+        }
+    }
+}
---
+++ day 10/part2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adapterJoltages = ConsoleHelpers
+                .ReadInput()
+                .Select(int.Parse)
+                .ToList();
+            
+            adapterJoltages.Add(adapterJoltages.Max() + 3);
+            adapterJoltages.Add(0);
+            adapterJoltages.Sort();
+
+            var answer = Enumerable
+                .Range(0, adapterJoltages.Count - 1)
+                .Reverse()
+                .ToList()
+                .Aggregate(
+                    new Dictionary<int, long> { [adapterJoltages.Count - 1] = 1 },
+                    (connections, i) => {
+                        connections[i] = Enumerable
+                            .Range(i + 1, adapterJoltages.Count - i - 1)
+                            .TakeWhile(j => adapterJoltages[j] - adapterJoltages[i] <= 3)
+                            .Aggregate(0L, (acc, j) => acc + connections[j]);
+
+                        return connections;
+                    }
+                )
+                .GetValueOrDefault(0);
+
+            Console.WriteLine(answer);
+        }
+    }
+}
