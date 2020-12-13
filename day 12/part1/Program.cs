d6fe6aaf226692286a737c9acd967dab6be151c8
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-12 22:43:26 -0500

    day 12

---
+++ day 12/part1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ConsoleHelpers.ReadInput();
+
+            var ship = new Ship();
+
+            foreach (var line in lines)
+            {
+                var instruction = line[0];
+                var distance = int.Parse(string.Join("", line.Skip(1)));
+
+                ship.Move(instruction, distance);
+            }
+
+            Console.WriteLine(Math.Abs(ship.X) + Math.Abs(ship.Y));
+        }
+    }
+
+    public class Ship
+    {
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+        public CardinalDirection Direction { get; set; } = CardinalDirection.East;
+
+        public enum CardinalDirection
+        {
+            North = 0,
+            East = 1,
+            South = 2,
+            West = 3
+        }
+
+        public void Move(char instruction, int distance)
+        {
+            switch (instruction)
+            {
+                case 'N': MoveDirection(CardinalDirection.North, distance); break;
+                case 'S': MoveDirection(CardinalDirection.South, distance); break;
+                case 'E': MoveDirection(CardinalDirection.East, distance); break;
+                case 'W': MoveDirection(CardinalDirection.West, distance); break;
+                case 'L': {
+                    var quarterTurns = distance / 90;
+                    for (var i = 0; i < quarterTurns; i++)
+                    {
+                        Direction = Direction == CardinalDirection.North ? CardinalDirection.West : Direction - 1;
+                    }
+
+                    break;
+                }
+                case 'R': {
+                    var quarterTurns = distance / 90;
+                    for (var i = 0; i < quarterTurns; i++)
+                    {
+                        Direction = Direction == CardinalDirection.West ? CardinalDirection.North : Direction + 1;
+                    }
+                    
+                    break;
+                }
+                case 'F': {
+                    MoveDirection(Direction, distance);
+                    break;
+                }
+
+            }
+        }
+
+        private void MoveDirection(CardinalDirection direction, int distance)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.North: Y -= distance; break;
+                case CardinalDirection.South: Y += distance; break;
+                case CardinalDirection.East: X += distance; break;
+                case CardinalDirection.West: X -= distance; break;
+            }
+        }
+    }
+}
---
+++ day 12/part2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ConsoleHelpers.ReadInput();
+
+            var ship = new Ship();
+
+            foreach (var line in lines)
+            {
+                var instruction = line[0];
+                var distance = int.Parse(string.Join("", line.Skip(1)));
+
+                ship.Move(instruction, distance);
+            }
+
+            Console.WriteLine(Math.Abs(ship.X) + Math.Abs(ship.Y));
+        }
+    }
+
+    public class Ship
+    {
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+
+        public int WaypointX { get; set; } = 10;
+        public int WaypointY { get; set; } = -1;
+        public void Move(char instruction, int distance)
+        {
+            switch (instruction)
+            {
+                case 'N': WaypointY -= distance; break;
+                case 'S': WaypointY += distance; break;
+                case 'E': WaypointX += distance; break;
+                case 'W': WaypointX -= distance; break;
+                case 'L': {
+                    RotateWaypoint(-1 * distance);
+                    break;
+                }
+                case 'R': {
+                    RotateWaypoint(distance);
+                    break;
+                }
+                case 'F': {
+                    X += WaypointX * distance;
+                    Y += WaypointY * distance;                    
+                    break;
+                }
+
+            }
+        }
+
+        private void RotateWaypoint(int degrees)
+        {
+            var theta = degrees * Math.PI / 180;
+            var x = WaypointX;
+            var y = WaypointY;
+
+            WaypointX = (int) Math.Round(x * Math.Cos(theta) - y * Math.Sin(theta));
+            WaypointY = (int) Math.Round(x * Math.Sin(theta) + y * Math.Cos(theta));
+        }
+    }
+}
