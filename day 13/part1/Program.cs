7ff382585aed153051a7ceebe57d24cc13891d37
Brian Simon <bsssshhhhhhh@gmail.com>
2020-12-13 01:44:27 -0500

    day 13

---
+++ day 13/part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using AdventCommon;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ConsoleHelpers.ReadInput();
+            var timestamp = int.Parse(lines[0]);
+            var buses = lines[1]
+                .Split(",")
+                .Where(id => id != "x")
+                .Select(x => (int?)int.Parse(x));
+
+            for (var i = timestamp;;i++)
+            {
+                var bus = buses.FirstOrDefault(x => i % x.Value == 0);
+                if (bus.HasValue)
+                {
+                    Console.WriteLine(bus.Value * (i - timestamp));
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ day 13/part2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AdventCommon;
+using System.Linq;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ConsoleHelpers.ReadInput();
+            var buses = lines[1]
+                .Split(",")
+                .ToArray();
+
+            var product = 1L;
+            var timestamp = 100000000000000L;
+
+            for (var i = 0L; i < buses.Length; i++)
+            {
+                if (buses[i] == "x")
+                {
+                    continue;
+                }
+
+                var bus = int.Parse(buses[i]);
+
+                if ((timestamp + i) % bus == 0)
+                {
+                    product *= bus;
+                }
+                else
+                {
+                    timestamp += product;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(timestamp);
+        }
+    }
+}
